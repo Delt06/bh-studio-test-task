ba742fb6ba3901d75afa055278e562d1947a6281
Vladislav Kantaev <vvkantaev@yandex.ru>
2022-07-16 13:55:46 +0300

    Add third-person camera

---
+++ Assets/Scripts/CameraTarget.cs
@@ -0,0 +1,20 @@
+﻿using Mirror;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class CameraTarget : NetworkBehaviour
+{
+    public override void OnStartLocalPlayer()
+    {
+        base.OnStartLocalPlayer();
+
+        var cam = Camera.main;
+        Assert.IsNotNull(cam);
+
+        var thirdPersonCamera = cam.GetComponent<ThirdPersonCamera>();
+        Assert.IsNotNull(thirdPersonCamera);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        thirdPersonCamera.BindTo(transform);
+    }
+}
